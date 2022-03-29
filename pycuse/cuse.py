96e8e44bf82d1d90ca8c94a98a296123746ca1ff
Jevin Sweval <jevinsweval@gmail.com>
2022-03-29 14:53:31 -0700

    wip

--- pycuse/cuse.py
+++ pycuse/cuse.py
@@ -1,8 +1,14 @@
-from typing import Final
+from typing import Final, Sequence
 
 from ._cuse import ffi, lib
 
 CUSE_UNRESTRICTED_IOCTL: Final[int] = 1 << 0
+
+
+def cstr_array(strs: Sequence[str]):
+    cstrs = [ffi.new("char[]", s.encode("utf-8")) for s in strs]
+    res = ffi.new(f"char*[{len(strs)}]", cstrs)
+    return res
 
 
 class CUSEDev:
@@ -12,3 +18,12 @@ class CUSEDev:
             args.append("-f")
         if debug:
             args.append("-d")
+        ci = ffi.new("struct cuse_info *")
+        print(ci)
+        cargs = cstr_array(args)
+        print(cargs)
+        ci.flags = CUSE_UNRESTRICTED_IOCTL
+        ci.dev_info_argc = 1
+        ci.dev_info_argv = cstr_array([f"DEVNAME={name}"])
+        ops = ffi.new("struct cuse_lowlevel_ops *")
+        lib.cuse_lowlevel_main(len(args), cargs, ci, ops, ffi.NULL)
